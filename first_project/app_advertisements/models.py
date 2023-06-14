b0aad470c2e9327d41b1b99a8c6648d5c640e147
Maxim_Malakhov <2noocytrebul@gmail.com>
2023-06-14 20:24:00 +0700

    Homework for lesson 8

--- first_project/app_advertisements/models.py
+++ first_project/app_advertisements/models.py
@@ -1,4 +1,6 @@
+from django.contrib import admin
 from django.db import models
+from django.utils.html import format_html
 
 # Create your models here.
 
@@ -9,7 +11,27 @@ class Advertisement(models.Model):
     auction = models.BooleanField("торг", help_text="Отметьте, если торг уместен ;)")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
+    @admin.display(description="Дата создания")
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_at = self.created_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_at
+            )
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+        
+    @admin.display(description="Дата обновления")
+    def updated_date(self):
+        from django.utils import timezone
+        if self.updated_at.date() == timezone.now().date():
+            updated_at = self.created_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: limegreen; font-weight: bold;">Сегодня в {}</span>', updated_at
+            )
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
+
     def __str__(self):
         return "{}(id={id}, title={title}, price={price})".format(self.__class__.__name__, **self.__dict__)
     
